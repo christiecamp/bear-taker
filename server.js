6f0a1684b30f75a91680095adeb188e5967c5037
Christine Campbell <christiecamp@Christines-MacBook-Pro.local>
2023-11-04 10:03:50 -0400

    updating comments in server.js

--- server.js
+++ server.js
@@ -8,17 +8,21 @@ const app = express();
 //specify which port the server will run on
 const PORT = 3001; //placeholder
 
-
-
-
-
 //static middleware pointing to the public folder
 app.use(express.static('public'));
 
 //route file - placeholder
 app.require('.routes/routes.js');
 
+// app.get() is a method that allows express to respond to requests from the client. 
+// Each routing method corresponds to an HTTP method of the same name. ]
+// res.sendFile() allows you to send static files to the client. It accepts an argument of root and an optional options array.
+
+// app.get('/send', (req, res) =>
+//     res.sendFile(path.join('public/sendFile.html')));
+
+
 //listen() method is responsible for listening to incoming connectings on the specified port
 app.listen(PORT, () =>
-    console.log('Application listening on PORT')
+    console.log('Application listening at PORT')
 );
