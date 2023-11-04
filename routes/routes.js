61d09dab2f4b43101d974ea7081d58dc0b6d22d9
Christine Campbell <christiecamp@Christines-MacBook-Pro.local>
2023-11-04 10:25:45 -0400

    initial skeleton code for routes

---
+++ routes/routes.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+
+
+//all variables and routes
+
+//api routes
+    //get route
+    //post route
+
+//retrive note
+
+//delete note
+
+//display note
