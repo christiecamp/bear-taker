a296b0d78cf48633319ff3f842e59baf55b9e6fe
Christine Campbell <christiecamp@Christines-MacBook-Pro.local>
2023-11-03 20:25:41 -0400

    added routes file and placement pathing in server

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 //import express package
 const express = require('express');
-
 //import built-in Node.js package 'path to respolve path of files that are located on the server
+const fs = require('fs');
 const path = require('path');
-
 //initialize an instance of express
 const app = express();
-
 //specify which port the server will run on
 const PORT = 3001; //placeholder
+
+
+
+
 
 //static middleware pointing to the public folder
 app.use(express.static('public'));
 
-//route file
+//route file - placeholder
+app.require('.routes/routes.js');
 
 //listen() method is responsible for listening to incoming connectings on the specified port
 app.listen(PORT, () =>
