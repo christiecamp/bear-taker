1fbfa0a8d6b47b0045e8ffac8b82207e8eddb780
Christine Campbell <christiecamp@Christines-MacBook-Pro.local>
2023-11-06 14:26:59 -0500

    set-up console.log for testing

--- routes/routes.js
+++ routes/routes.js
@@ -1,39 +1,45 @@
 const fs = require('fs');
 const path = require('path');
 const db = require('../db/db.json');
+const notes = require('../public/notes.html');
+const index = require('../public/index.html');
 
 
 //all variables and routes
 
-//GET route for notes
+//GET request for all notes
+//__dirname is an environment variable that tells you the absolute path of the directory containing the currently executing file - keeping as placement
 bear.get('/api/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, notes));
     //reads and sends JSON response 
     res.json(db);
+    console.info(`${req.method} request received view notes`);
 });
 
 //POST route for notes
 bear.post('/api/notes', (req,res) => {
-
+    console.info(`${req.method} request received to post a new note`);
 });
 
 //GET route that returns a specific note
 bear.get('/api/notes/:note', (req,res) => {
     res.json(notes[req.params.note]);
+    console.info(`${req.method} request received to view specific note`);
 });
 
 //deletes specific note
 bear.delete('/api/notes/:note', (req, res) => {
-    console.log('Note deleted!');
+    console.info(`${req.method} request received to delete note`);
 });
 
 //display notes.html
 bear.get('notes', (req, res) => {
-    res.sendFile(path.join('../public/notes.html'));
+    res.sendFile(path.join(__dirname, notes));
 })
 
-//display index.html when other routes don't exist
+//display index.html when other routes don't exist 
 bear.get('*', (req,res) => {
-    res.sendFile(path.join('../public/index.html'));
+    return res.sendFile(path.join(__dirname, index));
 });
 
 
--- server.js
+++ server.js
@@ -12,19 +12,13 @@ const bear = express();
 const PORT = 3001; //placeholder
 
 
-
-//static middleware pointing to the public folder
+//static middleware pointing to public folder
+//parsing incoming JSON data
 bear.use(express.static('public'));
 bear.use(express.json());
 
 //route file - placeholder
 bear.require('.routes/routes.js');
-
-// app.get() is a method that allows express to respond to requests from the client. 
-// Each routing method corresponds to an HTTP method of the same name. ]
-// res.sendFile() allows you to send static files to the client. It accepts an argument of root and an optional options array.
-// bear.get('/send', (req, res) =>
-//     res.sendFile(path.join('public/sendFile.html')));
 
 
 //listen() method is responsible for listening to incoming connectings on the specified port
