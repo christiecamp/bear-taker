bf1665afaa1388554f2653fefbc39802ae4516e9
Christine Campbell <christiecamp@Christines-MacBook-Pro.local>
2023-11-07 18:47:32 -0500

    testing in heroku

--- server.js
+++ server.js
@@ -1,25 +1,22 @@
 //import express package
 const express = require('express');
-//point server to route file
-const panda = require('./routes/index.js');
-
+//initialize express app
+const bear = express();
 //specify port
 const PORT = process.env.PORT || 3005;
-//initialize express
-const bear = express();
-
 
 //parsing incoming JSON data & urlencoded data
-bear.use(express.json());
 bear.use(express.urlencoded({ extended: true }));
-
-//routes
-require('/api', panda)(bear);
-
+bear.use(express.json());
 //host public folder
 bear.use(express.static('public'));
 
+//point server to route file
+const panda = require('./routes/index.js');
+//routes
+bear.use('/api', panda);
+
+
 //listen() method is responsible for listening to incoming connectings on the specified port
-bear.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}!`);
-});
+bear.listen(PORT, () =>
+    console.log(`Server listening on port ${PORT}!`));
